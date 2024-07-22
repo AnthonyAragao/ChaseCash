90a9ddb5de825b68463597a426d4de986a61a2d0
Anthony Aragão <anthony.amoz.s.a@gmail.com>
2024-07-22 12:09:48 -0300

    feat: Adicionada validação de entrada para campos numéricos

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,20 @@
 import './bootstrap';
 import './sidebar';
 import './modal';
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const replaceValue = function() {
+        this.value = this.value.replace(/[^\d.,]/g, '');
+    };
+
+    const replaceValueOnlyNumbers = function(){
+        this.value = this.value.replace(/\D/g, '');
+    }
+
+    document.getElementById('custo').addEventListener('input', replaceValue);
+    document.getElementById('preco_venda').addEventListener('input', replaceValue);
+    document.getElementById('estoque').addEventListener('input', replaceValueOnlyNumbers);
+    document.getElementById('pontos').addEventListener('input', replaceValueOnlyNumbers);
+    document.getElementById('codigo').addEventListener('input', replaceValueOnlyNumbers);
+});
