c97ca1192312db032b1ecad0568322a9202be709
Anthony Aragão <anthony.amoz.s.a@gmail.com>
2024-07-29 14:06:24 -0300

    style: Adicionado card de etapas da venda e estilos relacionados

--- resources/views/vendas/form.blade.php
+++ resources/views/vendas/form.blade.php
@@ -8,10 +8,43 @@
         <div class="page-path">
             <span class="icon"><a href="#"><i class="fa-solid fa-home"></i></a></span>
             <span class="separator"><i class="fa-solid fa-angle-right"></i></span>
-            <span><a href="#">Vendas</a></span>
+            <span><a href="{{ route("vendas.index") }}">Vendas</a></span>
             <span class="separator"><i class="fa-solid fa-angle-right"></i></span>
             <span><a href="#" class="active">Nova venda</a></span>
         </div>
+    </div>
+</div>
+
+{{-- Card etapas --}}
+<div class="card-phases">
+    <div class="card-phases__stage">
+        <div class="card-phases__stage-progress progess-active">
+            <div></div>
+            <span><i class="fa-solid fa-circle-check"></i></span>
+            <div></div>
+        </div>
+
+        <span class="card-phases__stage-title title-active">Cliente</span>
+    </div>
+
+    <div class="card-phases__stage">
+        <div class="card-phases__stage-progress progess-active">
+            <div></div>
+            <span><i class="fa-solid fa-circle-check"></i></span>
+            <div></div>
+        </div>
+
+        <span class="card-phases__stage-title title-active">Itens compra</span>
+    </div>
+
+    <div class="card-phases__stage">
+        <div class="card-phases__stage-progress">
+            <div></div>
+            <span><i class="fa-regular fa-circle"></i></span>
+            <div></div>
+        </div>
+
+        <span class="card-phases__stage-title">Pagamento</span>
     </div>
 </div>
 @endsection
