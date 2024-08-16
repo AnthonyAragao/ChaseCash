3b9c48804fb1de260f59a6e45b3a8d88f49756c1
AnthonyAragao <anthony.amoz.s.a@gmail.com>
2024-08-15 22:50:19 -0300

    style: modificado passos do cadastrp(falta finalizar)

--- resources/views/vendas/form.blade.php
+++ resources/views/vendas/form.blade.php
@@ -15,8 +15,125 @@
     </div>
 </div>
 
-{{-- Card etapas --}}
-<div class="card-phases">
+
+<style>
+.step-wizard-list{
+    background: #f5f5f5;
+    box-shadow: 0 15px 25px rgba(0,0,0,0.1);
+    color: #333;
+    list-style-type: none;
+    border-radius: 10px;
+    display: flex;
+    padding: 20px 10px;
+    position: relative;
+    z-index: 10;
+}
+
+.step-wizard-item{
+    padding: 0 20px;
+    flex-basis: 0;
+    -webkit-box-flex: 1;
+    -ms-flex-positive:1;
+    flex-grow: 1;
+    max-width: 100%;
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+    min-width: 170px;
+    position: relative;
+}
+.step-wizard-item + .step-wizard-item:after{
+    content: "";
+    position: absolute;
+    left: 0;
+    top: 19px;
+    background: rgb(79, 70, 229);
+    width: 100%;
+    height: 2px;
+    transform: translateX(-50%);
+    z-index: -10;
+}
+.progress-count{
+    height: 40px;
+    width:40px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 50%;
+    font-weight: 600;
+    margin: 0 auto;
+    position: relative;
+    z-index:10;
+    color: transparent;
+}
+.progress-count:after{
+    content: "";
+    height: 40px;
+    width: 40px;
+    background: rgb(79, 70, 229);
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    border-radius: 50%;
+    z-index: -10;
+}
+.progress-count:before{
+    content: "";
+    height: 10px;
+    width: 20px;
+    border-left: 3px solid #fff;
+    border-bottom: 3px solid #fff;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -60%) rotate(-45deg);
+    transform-origin: center center;
+}
+.progress-label{
+    font-size: 14px;
+    font-weight: 600;
+    margin-top: 10px;
+}
+.current-item .progress-count:before,
+.current-item ~ .step-wizard-item .progress-count:before{
+    display: none;
+}
+.current-item ~ .step-wizard-item .progress-count:after{
+    height:10px;
+    width:10px;
+}
+.current-item ~ .step-wizard-item .progress-label{
+    opacity: 0.5;
+}
+.current-item .progress-count:after{
+    background: #fff;
+    border: 2px solid rgb(79, 70, 229);
+}
+.current-item .progress-count{
+    color: rgb(79, 70, 229);
+}
+</style>
+
+{{-- Section etapas --}}
+<section>
+    <ul class="step-wizard-list">
+        <li class="step-wizard-item">
+            <span class="progress-count">1</span>
+            <span class="progress-label">Cliente</span>
+        </li>
+        <li class="step-wizard-item">
+            <span class="progress-count">2</span>
+            <span class="progress-label">Itens compra</span>
+        </li>
+        <li class="step-wizard-item current-item">
+            <span class="progress-count">3</span>
+            <span class="progress-label">Pagamento</span>
+        </li>
+    </ul>
+</section>
+
+{{-- <div class="card-phases">
     <div class="card-phases__stage" id="stage-1">
         <div class="card-phases__stage-progress">
             <div></div>
@@ -46,7 +163,7 @@
 
         <span class="card-phases__stage-title">Pagamento</span>
     </div>
-</div>
+</div> --}}
 
 {{-- Formulário etapas --}}
 <div class="card-form-phases">
@@ -137,7 +254,7 @@
         const produtoSelect = document.getElementById('produto');
         const tbody = document.querySelector('tbody');
 
-        const produtos = @json($produtos); 
+        const produtos = @json($produtos);
 
         produtoSelect.addEventListener('change', function() {
             const produtoId = this.value;
